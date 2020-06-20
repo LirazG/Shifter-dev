daa04b796777982ea87bf450c0282e5f4c381d2c
Liraz Gur Arie <lirazgurarie@walla.com>
2020-06-20 17:49:22 +0300

    a

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,2 +1,2 @@
 export const AUTH_COOKIE = 'shifter_user';
-export const API_URL = 'http://localhost:5000';
+export const API_URL = process.env.NODE_ENV === 'production' ? 'https://shifterbiz.herokuapp.com' : 'http://localhost:5000';
